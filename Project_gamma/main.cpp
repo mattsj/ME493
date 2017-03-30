8e9f10aac8bb8f38d5b58c12fd8c5118106c23e2
mattsj <mjgb99@gmail.com>
2017-03-30 16:10:05 -0700

    Add files via upload

--- Project_gamma/main.cpp
+++ Project_gamma/main.cpp
@@ -72,18 +72,38 @@ public:
 	void mutate();
 };
 
-//swaps the city order of two cities 
+//swaps the city order of two cities, can't swap the starting city
 void policy::mutate() {
-	cout << "mutate" << endl;
+	//cout << "mutate" << endl;
+	//cout << "size = " << order.size() << endl;
 	int rand1 = rand() % order.size();
+	if (rand1 == 0) {
+		rand1 = rand1 + 1;
+	}
 	int rand2 = rand() % order.size();
+	if (rand2 == 0) {
+		rand2 = rand2 + 1;
+	}
 	while (rand1 == rand2) {
 		rand2 = rand() % order.size();
+		if (rand2 == 0) {
+			rand2 = rand2 + 1;
+		}
 	}
+	/*cout << "r1 = " << rand1 << endl;
+	cout << "r2 = " << rand2 << endl;
+	for (int i = 0; i < order.size(); i++) {
+		cout << order.at(i) << endl;
+	}*/
+	//cout << "break" << endl;
 	int temp;
 	temp = order.at(rand1);
+	//cout << "temp = " << temp << endl;
+	//cout << "1 = "<<order.at(rand1) << endl;
+	//cout << "2 = "<<order.at(rand2) << endl;
 	order.at(rand1) = order.at(rand2);
 	order.at(rand2) = temp;
+	//cout << "done mutate" << endl;
 }
 
 //only for initial guesses
@@ -92,13 +112,8 @@ int pick_city(vector<city> vcity) {
 	int test;
 
 	t = rand() % vcity.size();//choose which city to move to
-	cout << "t = " << t << endl;
-	cout << "v = " << vcity.at(t).visit << endl;
-	while (vcity.at(t).visit = 1) {
-		cin >> test;
+	while (vcity.at(t).visit == 1) {
 		t = rand() % vcity.size();
-		cout << "t = " << t << endl;
-		cout << "v = " << vcity.at(t).visit << endl;
 	}
 	return t;
 }
@@ -110,7 +125,7 @@ vector<city> move(agent* pagent, vector<city> vcity, int picked_city) {
 }
 
 //only for mutating
-void mutate_move(agent* pagent, vector<city> vcity, vector<policy> vpolicy) {
+double mutate_move(agent* pagent, vector<city> vcity, vector<policy> vpolicy) {
 	pagent->distance = 0;
 	pagent->total_distance = 0;
 	for (int i = 0; i < vpolicy.size() - 1; i++) {
@@ -125,44 +140,46 @@ void mutate_move(agent* pagent, vector<city> vcity, vector<policy> vpolicy) {
 		city1++;
 		city2++;
 	}
+	return pagent->total_distance;
 }
 
 //resets agent and cities to be unvisited
-int start(vector<city> vcity, int start_city, agent* pagent) {
-	for (int i = 0; i < vcity.size(); i++) {
-		vcity.at(i).visit = 0;
-	}
-	vcity.at(start_city).visit = 1;
-
-	pagent->agentX = vcity.at(start_city).cityX;
-	pagent->agentY = vcity.at(start_city).cityY;
-	pagent->distance = 0;
-	pagent->total_distance = 0;
-
-	return vcity.at(start_city).visit;
-}
+//int start(vector<city> vcity, int start_city, agent* pagent) {
+//	for (int i = 0; i < vcity.size(); i++) {
+//		vcity.at(i).visit = 0;
+//	}
+//	vcity.at(start_city).visit = 1;
+//
+//	pagent->agentX = vcity.at(start_city).cityX;
+//	pagent->agentY = vcity.at(start_city).cityY;
+//	pagent->distance = 0;
+//	pagent->total_distance = 0;
+//
+//	return vcity.at(start_city).visit;
+//}
 
 //creates  copy of a policy and mutates
 vector<policy> replicate(vector<policy> vP, agent* pagent, vector<city> vcity) {
-	vector<policy> pop;
+	//cout << "replicate" << endl;
+	vector<policy> pop;//new vector of policies
 	pop = vP;
 	while (pop.size() < vP.size() * 2) {
 		int spot = rand() % pop.size();
 		policy p;
 		p = pop.at(spot);
 		p.mutate();
-		mutate_move(pagent,vcity, pop);
+		p.total_d = mutate_move(pagent, vcity, pop);
 		pop.push_back(p);
 	}
-
+	assert(pop.size() == vP.size() * 2);
 	return pop;
 }
 
 //reduces amount of policies
 vector<policy> downselect(vector<policy> vP) {
-	cout << "downselect" << endl;
+	//cout << "downselect" << endl;
 	//binary touranment
-	vector<policy> population;
+	vector<policy> population;//new vector of policies
 	policy p;
 	while (population.size() < vP.size() / 2) {
 		int spot1 = rand() % vP.size();
@@ -176,13 +193,13 @@ vector<policy> downselect(vector<policy> vP) {
 
 		if (fit1 < fit2) {
 			p = vP.at(spot1);
-
 		}
 		else {
 			p = vP.at(spot2);
 		}
 		population.push_back(p);
 	}
+	assert(vP.size() / 2 == population.size());
 	return population;
 }
 
@@ -202,10 +219,8 @@ int main() {
 		//checks for cities in same loc
 		if (a >= 1) {
 			for (int g = 0; g < vcity.size(); g++) {
-				while (town.cityX == vcity.at(g).cityX) {
-					while (town.cityY == vcity.at(g).cityY) {
-						town.init();
-					}
+				while ((town.cityX == vcity.at(g).cityX) && (town.cityY == vcity.at(g).cityY)) {
+					town.init();
 				}
 			}
 		}
@@ -214,6 +229,9 @@ int main() {
 		count++;
 	}
 	assert(num_cities == vcity.size());
+	for (int z = 0; z < vcity.size(); z++) {
+		cout << "x = " << vcity.at(z).cityX << " y = " << vcity.at(z).cityY << " visit = " << vcity.at(z).visit << endl;
+	}
 
 	policy P;
 	vector<policy> vpolicy;
@@ -222,41 +240,77 @@ int main() {
 	int start_city = 0;//city where the agent will start
 	int chosen_city;
 
-	int generations = 1;
-	int iterations = 2;
+	int generations = 5;//number of RED cycles
+	int num_policies = 10;//number of total policies
+
 	//initial guesses
 	cout << "begin guess" << endl;
-	for (int e = 0; e < iterations; e++) {
-		vcity.at(start_city).visit = start(vcity, start_city, psmith);
-		for (int z = 0; z < vcity.size(); z++) {
-			cout << "x = " << vcity.at(z).cityX << " y = " << vcity.at(z).cityY << " visit = " << vcity.at(z).visit << endl;
+	for (int e = 0; e < num_policies; e++) {
+		//reset agent and cities
+		for (int p = 0; p < vcity.size(); p++) {
+			vcity.at(p).visit = 0;
 		}
+		vcity.at(start_city).visit = 1;
+		smith.agentX = vcity.at(start_city).cityX;
+		smith.agentY = vcity.at(start_city).cityY;
+		smith.distance = 0;
+		smith.total_distance = 0;
+		/*for (int z = 0; z < vcity.size(); z++) {
+			cout << "x = " << vcity.at(z).cityX << " y = " << vcity.at(z).cityY << " visit = " << vcity.at(z).visit << endl;
+		}*/
+		//cout << endl;
 		P.order.clear();
 		P.total_d = 0;
-		cout << "guess" << endl;
+		P.order.push_back(vcity.at(start_city).number);
+
+		//guesses
+		//cout << "guess" << endl;
+		int gar;
+		//cin >> gar;
 		for (int f = 0; f < num_cities-1; f++) {
 			chosen_city = pick_city(vcity);
 			vcity = move(psmith, vcity, chosen_city);
 			smith.calc_d(); 
-			cout << "d = " << smith.distance << endl;
+			//cout << "d = " << smith.distance << endl;
 			P.order.push_back(vcity.at(chosen_city).number);
-			cout << "3333" << endl;
 		}
-		cout << "2222" << endl;
 		P.total_d = smith.total_distance;
 		vpolicy.push_back(P);
 
 	}
 	cout << "finished guess" << endl;
-	assert(vpolicy.size() == iterations);
+	assert(vpolicy.size() == num_policies);
 
 	//generation
 	for (int current_g = 0; current_g < generations; current_g++) {
 		//replicate/mutate
 		vpolicy = replicate(vpolicy, psmith, vcity);
+			//for (int j = 0; j < vpolicy.at(0).order.size(); j++) {
+			//	//cout << vpolicy.at(i).order.at(j) << endl;
+			//}
+			//cout << endl;
 		//downselect
 		vpolicy = downselect(vpolicy);
+		for (int i = 0; i < vpolicy.size(); i++) {
+			cout << vpolicy.at(i).total_d << endl;
+		}
+		cout << endl;
 	}
+	assert(vpolicy.size() == num_policies);
 
+	double min_value = 1000000000;
+	int min_loc;
+	for (int r = 0; r < vpolicy.size(); r++) {
+		cout << "d = " << vpolicy.at(r).total_d << endl;
+		if (vpolicy.at(r).total_d < min_value) {
+			min_value = vpolicy.at(r).total_d;
+			min_loc = r;
+		}
+	}
+	cout << "min loc = " << min_loc << endl;
+	cout << "total d = " << min_value << endl;
+	for (int w = 0; w < vpolicy.at(min_loc).order.size(); w++) {
+		cout << vpolicy.at(min_loc).order.at(w) << endl;
+	}
 	return 0;
 }
